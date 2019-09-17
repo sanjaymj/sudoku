169420b9d935fd47b4ddfbfd838b1090950b1178
sanjay <Sanjay.Jayaram@clear-it.de>
2019-09-17 14:59:01 +0200

    *Add a service for creating random sudoku board
    *Include a difficulty level

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { SudokuService, GameState } from 'src/app/services/sudoku.service';
+import { DifficultyLevel } from 'src/app/services/difficulty.level.service';
 
 @Component({
   selector: 'app-game',
@@ -11,12 +12,17 @@ export class GameComponent implements OnInit {
   constructor(public sudokuService: SudokuService) { }
 
   public gameState = GameState;
+
+  @Input()
+  gameDifficulty:DifficultyLevel;
+
+  public difficultyLevel = [{value: DifficultyLevel.Easy, id: 'Easy'}, {value: DifficultyLevel.Medium, id: 'Medium'}, {value: DifficultyLevel.Hard, id: 'Hard'}];
   
   ngOnInit() {
   }
 
   public startGame() {
-    this.sudokuService.startGame();
+    this.sudokuService.startGame(this.gameDifficulty);
   }
 
   public stopGame() {
---
+++ src/app/services/difficulty.level.service.ts
@@ -0,0 +1,5 @@
+export enum DifficultyLevel {
+    Easy = 1,
+    Medium = 2,
+    Hard = 3
+}
